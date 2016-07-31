1cb02bf6adb64777344356657a11a9e9633c474c
summerohoh <jo289@cornell.edu>
2016-07-31 16:39:02 +0900

    Initial Commit

---
+++ CalculatorApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorApp
+//
+//  Created by Jiyoung Oh on 7/31/16.
+//  Copyright © 2016 Summer Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatorApp
+//
+//  Created by Jiyoung Oh on 7/31/16.
+//  Copyright © 2016 Summer Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
